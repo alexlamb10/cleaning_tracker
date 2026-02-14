84424cf7819a5d3392c73622a03c71cd20e9f0fb
Alex Lamb <alexlamb104@gmail.com>
2026-02-14 10:07:03 -0700

    added logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:cleaning_tracker/services/data_service.dart';
 import 'package:cleaning_tracker/screens/dashboard_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => DataService(),
